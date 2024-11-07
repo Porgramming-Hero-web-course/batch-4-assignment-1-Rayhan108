3cdc980bc800b43ab281c53021ad7e960f01de5c
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-11-07 15:21:42 +0600

    chnange num to nums variable

--- Problems/src/problem-2.ts
+++ Problems/src/problem-2.ts
@@ -1,14 +1,8 @@
 {
     // Create a TypeScript function removeDuplicates that accepts an array of numbers and returns a new array with duplicates removed. Preserve the original order of elements.
 
-// // Sample Input:
-// removeDuplicates([1, 2, 2, 3, 4, 4, 5])
-
-// // Sample Output:
-// [1, 2, 3, 4, 5]
-
 function removeDuplicates(nums:number[]):number[]{
-    return nums.filter((num:number,index:number)=>num.indexOf(num) ===index)
+    return nums.filter((num:number,index:number)=>nums.indexOf(num) ===index)
 }
 
 console.log(removeDuplicates([1,2,2,3,4,4,5]));
