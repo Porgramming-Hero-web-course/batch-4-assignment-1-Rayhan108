40e60ff00b04a46c6f9efca9c44c1f4e98b28772
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-11-07 16:27:11 +0600

    problem 5

---
+++ Problems/src/problem-5.ts
@@ -0,0 +1,15 @@
+{
+//Write a generic function getProperty that takes an object and a property name as arguments and returns the property value. Add a constraint to ensure the property name exists on the object.
+
+
+const getProperty=<T extends {propertyName:string}>(obj:T,propertyName:string)=>{
+return {};
+}
+
+
+
+
+
+
+
+}
